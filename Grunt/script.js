ca2a8b67fb879d6cd37239added11e72cdc16f2c
JOSE RAFAEL RAMON NAVARRO <jrafa.ramon@gmail.com>
2015-08-02 21:53:37 +0200

    Grunt Example

---
+++ Grunt/script.js
@@ -0,0 +1,26 @@
+//Modelo Alumnos
+function AlumnosController($scope){
+	$scope.alumnos=[
+		{nombre: "Rafa RN", telefono: "123456789", curso: "Primero de Angular"},
+		{nombre: "Fer RN",  telefono: "123456789", curso: "Primero de Angular"},
+		{nombre: "Vir RN",  telefono: "123456789", curso: "Primero de Angular"},
+		{nombre: "Luis RN", telefono: "123456789", curso: "Primero de Angular"}
+	];
+	
+	$scope.Save = function(){
+		$scope.alumnos.push({
+			nombre:$scope.nuevoAlumno.nombre,
+			telefono:$scope.nuevoAlumno.telefono,
+			curso:$scope.nuevoAlumno.curso,
+		});
+		$scope.formVisibility = false;
+		console.log($scope.formVisibility);
+	};
+
+	$scope.formVisibility=false;
+	$scope.ShowForm = function(){
+		$scope.formVisibility=true;
+		console.log($scope.formVisibility);
+	};
+}
+
