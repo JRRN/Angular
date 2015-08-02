91873a403037652032c8496e19cba3b63a97cf88
JOSE RAFAEL RAMON NAVARRO <jrafa.ramon@gmail.com>
2015-08-02 19:56:51 +0200

    Añadir Alumno

--- Project/script.js
+++ Project/script.js
@@ -6,4 +6,12 @@ function AlumnosController($scope){
 		{nombre: "Vir RN",  telefono: "123456789", curso: "Primero de Angular"},
 		{nombre: "Luis RN", telefono: "123456789", curso: "Primero de Angular"}
 	];
+	
+	$scope.Save = function(){
+		$scope.alumnos.push({
+			nombre:$scope.nuevoAlumno.nombre,
+			nombre:$scope.nuevoAlumno.telefono,
+			nombre:$scope.nuevoAlumno.curso,
+		});
+	}
 }
