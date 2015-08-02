113637577c38707b0eafa342b6b4c6be2c8d7539
JOSE RAFAEL RAMON NAVARRO <jrafa.ramon@gmail.com>
2015-08-02 18:10:50 +0200

    Init Project

---
+++ Project/script.js
@@ -0,0 +1,9 @@
+//Modelo Alumnos
+function AlumnosController($scope){
+	$scope.alumnos=[
+		{nombre: "Rafa RN", telefono: "123456789", curso: "Primero de Angular"},
+		{nombre: "Fer RN",  telefono: "123456789", curso: "Primero de Angular"},
+		{nombre: "Vir RN",  telefono: "123456789", curso: "Primero de Angular"},
+		{nombre: "Luis RN", telefono: "123456789", curso: "Primero de Angular"}
+	];
+}
